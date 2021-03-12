90ff2aeb2bf467c4f2507e472ef56767d775a7ef
hunterpearson9 <50116812+hunterpearson9@users.noreply.github.com>
2021-03-12 15:28:00 -0700

    Add files via upload

--- MECH_307_Project_Code_RaspberryPI/MECH_307_Project_Code_RaspberryPI.py
+++ MECH_307_Project_Code_RaspberryPI/MECH_307_Project_Code_RaspberryPI.py
@@ -3,47 +3,53 @@ import cv2
 import serial
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") #set the face cascade file to a variable
-ser = serial.Serial('COM3',9600)
-pos = 0
+ser = serial.Serial('COM3',9600) # Serial communication initialization For Windows
+#ser = serial.Serial('/dev/ttyACM0',9600) # Serial communication initialization For Linux
+xPos = 0 #initialize x position variable
+yPos = 0 #Initialize y position variable
 
 width = 500 #desired video width in pixels
 height = 1080 #desired video height in pixels
-cap = cv2.VideoCapture(0)
-cap.set(3,width) #video width
-cap.set(4,height) #video height
+cap = cv2.VideoCapture(0) #initalize video capture
+cap.set(3,width) #set video width (pixels)
+cap.set(4,height) # set video height (pixels)
 width = cap.get(3) #re-sets variable for video width to actual video width (in pixels)
 height = cap.get(4) #re-sets variable for video height to actual video height (in pixels)
 
 #boxTargetSize = 100 #number of square pixels for target box
 boxPercent = 0.03 #Percent of the screen the targeting box takes up (Decimal form)
 boxTargetSize = pow(boxPercent * width * height,1 / 2) #Calculates number of square pixels for target box
-boxTargetPts1 = (int(width / 2 - boxTargetSize / 2),int(height / 2 - boxTargetSize / 2))
-boxTargetPts2 = (int(width / 2 + boxTargetSize / 2),int(height / 2 + boxTargetSize / 2))
-boxTargetCenter = 0
+boxTargetPts1 = (int(width / 2 - boxTargetSize / 2),int(height / 2 - boxTargetSize / 2)) #Calculate first point for target box
+boxTargetPts2 = (int(width / 2 + boxTargetSize / 2),int(height / 2 + boxTargetSize / 2)) #Calculate second point for target box
+#boxTargetCenter = 0
 
 stroke = 2 #Sets stroke size for shapes
+frameDelay = 10 # Sets the delay in ms between each frame.  1 is minimum
 while(True):
     ret, frame = cap.read()
+    #frame = cv2.flip(frame,0) #Flip frame upside down for Raspberry Pi Camera
     frameGray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(frameGray,1.2,4)
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x + w,y + h),(255,0,0),stroke) #Draw rectangle around face
-        cv2.circle(frame,(int(x + w / 2),int(y + h / 2)),5,(255,0,0),stroke)
+        cv2.rectangle(frame,(x,y),(x + w,y + h),(255,0,0),stroke) #Draw rectangle around a face
+        cv2.circle(frame,(int(x + w / 2),int(y + h / 2)),5,(255,0,0),stroke) #draw a circle at the center of a face
 
     try:
         if faces[0,0] + (faces[0,2] / 2) > boxTargetPts1[0] and faces[0,1] + (faces[0,3] / 2) > boxTargetPts1[1] and faces[0,0] + (faces[0,2] / 2) < boxTargetPts2[0] and faces[0,1] + (faces[0,3] / 2) < boxTargetPts2[1]:
-                cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(0,255,0),stroke)
+                cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(0,255,0),stroke) #Draw green rectangle if face is within target area
         else:
-            cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(255,0,0),stroke)
-        pos = int(180 - 180 * (faces[0,0] + (faces[0,2] / 2)) / width) #Maps the center of face0 in the camera view to 180 degrees
-        #print(pos) #Display the position to be sent to arduino
-        ser.write(str(pos).encode('ascii')) #sends a serial string to the arduinio
+            cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(255,0,0),stroke) #draw a blue rectangle around target area
+        xPos = int(100 * (0.5 - (faces[0,0] + (faces[0,2] / 2)) / width)) #Maps the x-center of face0 in the camera view from -100 to 100
+        yPos = int(180 - 180 * (faces[0,1] + (faces[0,3] / 2)) / height) #Maps the y-center of face0 in the camera view to 180 degrees
+        posData = str(xPos) + " " + str(yPos) #Convert x and y position data into a string with a 'space' separator
+        print(posData) #Display the position to be sent to arduino
+        ser.write(str(posData).encode('ascii')) #sends a serial string to the arduinioP
     except:
         #print("No Face Found")
-        cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(0,0,255),stroke)
+        cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(0,0,255),stroke) #draw a red rectangle around target area
 
     cv2.imshow("frame", frame)
-    if cv2.waitKey(15) & 0xff == ord("q"):
+    if cv2.waitKey(frameDelay) & 0xff == ord("q"):
         break
 cap.release()
 cv2.destroyAllWindows()
