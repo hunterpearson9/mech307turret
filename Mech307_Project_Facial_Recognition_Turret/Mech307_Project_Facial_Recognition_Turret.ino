16bc44b5be48800ad88cfdb47ec593178d23d38a
hunterpearson9 <50116812+hunterpearson9@users.noreply.github.com>
2021-04-06 13:20:56 -0600

    Add files via upload

--- Mech307_Project_Facial_Recognition_Turret/Mech307_Project_Facial_Recognition_Turret.ino
+++ Mech307_Project_Facial_Recognition_Turret/Mech307_Project_Facial_Recognition_Turret.ino
@@ -4,56 +4,81 @@
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 const int oneDeg = stepsPerRevolution / 360 + 1; //converts steps per revolution into degrees per revolution
 
-const int in[] = {2, 3, 4, 5}; // Define stepper motor in-pins
-const int servopin[] = {10}; // Define servomotor pins
-const int buzzerPin = 11; // Define the buzzer pin
+const int in[] = {10, 11, 12, 13}; // Define stepper motor in-pins
+const int servopin[] = {9, 6}; // Define servomotor pins
+const int buzzerPin = 3; // Define the buzzer pin
+const int ledPin = 5; // Define the LED pin
+
+const int buttonPin = A5; // Define button pin
 
 bool fired = LOW; // tells the arduino if the gun has been fired or not
 
+int step1;
+int serv1;
+
 Servo servo1;  // create servo object to control servo1
+Servo servo2; // create servo object to control servo2
 Stepper stepper(stepsPerRevolution, in[0], in[2], in[1], in[3]); // create stepper object to control stepper motor
 
-int parsePos; //initialize parsePos variable - used to distinguish between x and y position data
+int parsePos1; //initialize parsePos variable - used to distinguish between x and y position data
+int parsePos2; //initialize parsePos variable - used to distinguish between x and y position data
 
 void setup()
 {
   servo1.attach(servopin[0]);  // attaches the servo on the first pin in the servopin array to the servo object
-  servo1.write(45); // sets servo1 initial position to 45 degrees
-  stepper.setSpeed(15); // sets stepper motor speed to 15 rpm
-  Serial.begin(9600); // initalize serial communication
+  servo2.attach(servopin[1]);  // attaches the servo on the first pin in the servopin array to the servo object
+  servo1.write(135); // sets servo1 initial position to 45 degrees
+  servo2.write(0); // sets servo0 initial position to 0 degrees
+  stepper.setSpeed(10); // max motor speed is 15 rpm
+  Serial.begin(19200); // initalize serial communication
 }
 
 void loop() {
-  if (Serial.available()) {
+  if (fired == HIGH) {
+    stateFired(); //run code for when the gun is in the "fired" state
+  }
+  else if (Serial.available()) {
     String str = "";
     while (Serial.available()) {
       str += ((char) Serial.read());
       delay(2);
     }
     if (str != "") {
-      parsePos = str.indexOf(" ");
-      Serial.println(str.substring(parsePos + 1, str.length()).toInt());
-      stepper.step(int(str.substring(0, parsePos).toInt()*oneDeg));
-      servo1.write(str.substring(parsePos + 1, str.length()).toInt()); // tell servo to go to position in variable 'pos'
-      delay(100);
+      parsePos1 = str.indexOf(" ");
+      parsePos2 = str.indexOf(" ", parsePos1 + 1);
+      step1 = int(str.substring(0, parsePos1).toInt());
+      if (abs(step1) > 5) { // tolerance for turning turret base
+        stepper.step(map(step1, -50, 50, -10, 10)*oneDeg); // map input to +/- 10 degrees
+      }
+      serv1=str.substring(parsePos1 + 1, parsePos2).toInt();
+      servo1.write(map(serv1, 0, 100, 180, 90));
+      if (str.substring(parsePos2 + 1, str.length()).toInt() >= 100) {
+        servo2.write(90);
+        analogWrite(buzzerPin, 255);
+        delay(150);
+        analogWrite(buzzerPin, LOW);
+        fired = HIGH;
+      }
+      //delay(100);
 
     }
   }
   else {
-    if (fired == HIGH) {
-      soundFired(); //activate buzzer
-    }
   }
 }
 
-void soundFired() {
-  analogWrite(buzzerPin, 1);
+void stateFired() {
+  analogWrite(ledPin, 255);
   delay(100);
-  analogWrite(buzzerPin, LOW);
-  for (int i = 1; i >= 29 ; i++) {
-    delay(100);
-    if (fired == LOW) {
-      i = 30;
+  analogWrite(ledPin, LOW);
+  for (int i = 1; i < 300 ; i++) {
+    delay(10);
+    if (analogRead(buttonPin) >= 1000) {
+      fired = LOW;
+      analogWrite(ledPin, LOW);
+      servo2.write(0);
+      i = 301;
+      while(Serial.available()){Serial.read();};
     }
   }
 }
