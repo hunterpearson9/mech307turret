96491ffae4166d615e2fdbcd380d17232ff4ee1f
hunterpearson9 <50116812+hunterpearson9@users.noreply.github.com>
2021-04-27 12:03:15 -0600

    Add files via upload

--- MECH_307_Project_Code_RaspberryPI/MECH_307_Project_Code_RaspberryPI.py
+++ MECH_307_Project_Code_RaspberryPI/MECH_307_Project_Code_RaspberryPI.py
@@ -30,7 +30,7 @@ while(True): #wait for serial connection
         print("serial connection successful")
         break
     except:
-        #print("connecting...")
+        print("connecting...")
         pass #do nothing
     #start facial recognition
 while(True):
@@ -43,17 +43,21 @@ while(True):
         #cv2.circle(frame,(int(x + w / 2),int(y + h / 2)),5,(255,0,0),stroke) #draw a circle at the center of a face
     try:
         #if faces[0,0] + (faces[0,2] / 2) > boxTargetPts1[0] and faces[0,1] + (faces[0,3] / 2) > boxTargetPts1[1] and faces[0,0] + (faces[0,2] / 2) < boxTargetPts2[0] and faces[0,1] + (faces[0,3] / 2) < boxTargetPts2[1]:
-        if faces[0,0] + (faces[0,2] / 2) > boxTargetPts1[0] and faces[0,0] + (faces[0,2] / 2) < boxTargetPts2[0]:    
+        #if faces[0,0] + (faces[0,2] / 2) > boxTargetPts1[0] and faces[0,0] + (faces[0,2] / 2) < boxTargetPts2[0]:    
             #cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(0,255,0),stroke) #Draw green rectangle if face is within target area
-            fire = fire + 5
-            if fire > 100:
-                fire = 0
-        else:
+        #    fire = fire + 5
+        #    if fire > 100:
+        #        fire = 0
+        #else:
             #cv2.rectangle(frame,boxTargetPts1,boxTargetPts2,(255,0,0),stroke)
             ##draw a blue rectangle around target area
-            fire = 0
+        #    fire = 0
         xPos = int(50 - 100 * (faces[0,0] + (faces[0,2] / 2)) / width) #Maps the x-center of face0 in the camera view from -50 to 50
         yPos = int(100 - 100 * (faces[0,1] + (faces[0,3] / 2)) / height) #Maps the y-center of face0 in the camera view from 0 to 100
+        if abs(xPos)<15:
+            fire=fire+10
+        else:
+            fire = 0
         posData = str(xPos) + " " + str(yPos)+" "+str(fire) #Convert x and y position data into a string with a 'space' separator
         print(posData) #Display the position to be sent to arduino
         ser.write(str(posData).encode('ascii')) #sends a serial string to the arduinioP
